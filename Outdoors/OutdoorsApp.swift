22ebc3311d670f5ab794229d944dc85b412927d1
Joshua Tolentino <j9256t@gmail.com>
2021-03-30 22:42:17 -0600

    Initial Commit

---
+++ Outdoors/OutdoorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutdoorsApp.swift
+//  Outdoors
+//
+//  Created by Joshua Tolentino on 2021-03-30.
+//
+
+import SwiftUI
+
+@main
+struct OutdoorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
