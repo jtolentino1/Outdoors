682b435500d8f780a02af96e2ae8f34d428a86bd
Joshua Tolentino <j9256t@gmail.com>
2021-04-01 00:59:38 -0600

    final version

---
+++ Outdoors/Model/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  Outdoors
+//
+//  Created by Joshua Tolentino on 2021-04-01.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+
+    static let `default` = Profile(username: "j_tolentino")
+
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+        var id: String { self.rawValue }
+    }
+}
