b3e9198d5ceb0854131919a8b863c7cbf00c87d1
Joshua Tolentino <j9256t@gmail.com>
2021-04-01 00:05:37 -0600

    updates

---
+++ Outdoors/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Outdoors
+//
+//  Created by Joshua Tolentino on 2021-03-31.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
---
+++ Outdoors/Views/Hikes/HikeDetail.swift
@@ -0,0 +1,47 @@
+//
+//  Landmark.swift
+//  Outdoors
+//
+//  Created by Joshua Tolentino on 2021-03-30.
+//
+
+import SwiftUI
+
+struct HikeDetail: View {
+    let hike: Hike
+    @State var dataToShow = \Hike.Observation.elevation
+
+    var buttons = [
+        ("Elevation", \Hike.Observation.elevation),
+        ("Heart Rate", \Hike.Observation.heartRate),
+        ("Pace", \Hike.Observation.pace)
+    ]
+
+    var body: some View {
+        return VStack {
+            HikeGraph(hike: hike, path: dataToShow)
+                .frame(height: 200)
+
+            HStack(spacing: 25) {
+                ForEach(buttons, id: \.0) { value in
+                    Button(action: {
+                        self.dataToShow = value.1
+                    }) {
+                        Text(value.0)
+                            .font(.system(size: 15))
+                            .foregroundColor(value.1 == self.dataToShow
+                                ? Color.gray
+                                : Color.accentColor)
+                            .animation(nil)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct HikeDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        HikeDetail(hike: ModelData().hikes[0])
+    }
+}
